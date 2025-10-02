03e42e94f7605c891e64fbfc672b965a9319620d
Juan Diaz <juanjh1>
2025-10-02 16:23:09 -0500

    feat/client demo

--- example_pthon/Pices.py
+++ example_pthon/Pices.py
@@ -4,15 +4,20 @@ from assets_loader import XPICE_IMG, OPICE_IMG
 class Pice():
     
     def __init__(self, symbol: str, img= None) -> None:
+        
         self.symbol = symbol
         self.img = img
 
     def __str__(self) -> str:
+        
         return self.symbol
+    
     def __repr__(self) -> str:
+        
         return self.symbol
   
     def get_image(self):
+        
         return self.img
 
 
@@ -22,20 +27,14 @@ class XPice(Pice) :
         super().__init__("X", XPICE_IMG)
     
 
-
-
-
 class OPice(Pice):
 
     def __init__(self):
         super().__init__("O", OPICE_IMG)
          
 
-
 class VoidPice(Pice):
 
     def __init__(self, ):
         super().__init__("-")
 
-    
-         
--- example_pthon/run.py
+++ example_pthon/run.py
@@ -41,53 +41,69 @@ pice_in_game = 0
 tie_conunter = 0
 
 while running:
+    
     for event in pygame.event.get():
+        
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+            break
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()  
             cartesian_x = 0
             cartesian_y = 1
             if(current_status == Status.INGAME):
              
                 if  (pos[cartesian_x] > main_rect.left and pos[cartesian_x] < MAIN_RECT_WIDTH + main_rect.left):
+                    
                     x = (pos[cartesian_x] - main_rect.left) // PIXEL_X
                     y = (pos[cartesian_y] - main_rect.top) // PIXEL_Y
+                    
                     if(table.validate_pice_oposite(x, y)):
                             current_pice = turn_dict[turn]
                             table.put_pice(x, y, current_pice[pice_in_game]) 
                             turn = current_pice[next_turn]
-                            tie_conunter += 1; 
-                                          
+                            tie_conunter += 1;                       
+                    
                     if table.validate_winner(XPice):
                         winer_img = XPICE_IMG
                         current_status = Status.WINNER
+                    
                     if table.validate_winner(OPice):
                         winer_img = OPICE_IMG
                         current_status = Status.WINNER
+                    
                     if tie_conunter == 9:
                          current_status = Status.WINNER
 
             if current_status == Status.MENU:
+                
                 if center_rects(WIDTH, 300) < pos[cartesian_x] and (center_rects(WIDTH, 300) < pos[cartesian_x] and pos[cartesian_y] > 90 and pos[cartesian_y] < 150 + 90 ):
                     current_status = Status.INGAME
+            
             if current_status == Status.WINNER:
+                
                 if center_rects(WIDTH, 300) < pos[cartesian_x] and (center_rects(WIDTH, 300) < pos[cartesian_x] and pos[cartesian_y] > 90 and pos[cartesian_y] < 150 + 90 ):
                     current_status = Status.MENU
 
 
     screen.fill("purple")
+    
     if(current_status == Status.MENU):
+        
         tie_conunter = 0
         screen.blit(START_IMG, (center_rects(WIDTH, 300), 90))
         table = Table()
 
     if(current_status == Status.INGAME):
+        
         screen.fill("blue", main_rect )
         draw_lines(main_rect,Table.SIZE, screen, table)
 
     if(current_status == Status.WINNER):
+        
         screen.blit(RESTART_IMG, (center_rects(WIDTH, 300), 90))
+        
         if(winer_img != None):
             screen.blit(WINNER_IMG, (center_rects(WIDTH, 300), 400))
             
@@ -99,4 +115,5 @@ while running:
     pygame.display.flip()
 
     clock.tick(60) 
+
 pygame.quit()
