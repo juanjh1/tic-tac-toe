e703b14a1db8da2b978e5890ba2359149d0c7e87
unknown <samuel.rojas@jala.university>
2025-10-12 21:30:29 -0500

    Primer commit

---
+++ client_gui/assets_loader.py
@@ -0,0 +1,9 @@
+import pygame 
+
+
+XPICE_IMG = pygame.image.load("./asset/Xpice.png")
+OPICE_IMG = pygame.image.load("./asset/0pice.png")
+START_IMG = pygame.image.load("./asset/Start.png")
+WINNER_IMG = pygame.image.load("./asset/winer.png")
+RESTART_IMG = pygame.image.load("./asset/restart.png")
+TIE_IMG = pygame.image.load("./asset/Tie.png")
